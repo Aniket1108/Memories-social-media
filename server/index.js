7f355fc006a755c8a856184d92f3e6e4aca9ae3e
aniket1108 <aniketgholap1995@gmail.com>
2021-10-03 16:34:12 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -13,8 +14,9 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
-const CONNECTION_URL = "mongodb+srv://memories:memories@cluster0.ibcgb.mongodb.net/MEMORIES?retryWrites=true&w=majority"
+const CONNECTION_URL = 'mongodb+srv://memories:memories@cluster0.ibcgb.mongodb.net/MEMORIES?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,10 @@
 import express from 'express';
 
-import { getPosts, getPostsBySearch, getPostsByCreator, getPost, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
+import { getPosts, getPostsBySearch, getPost, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
 
-router.get('/creator', getPostsByCreator);
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.get('/:id', getPost);
@@ -15,5 +14,4 @@ router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
 router.post('/:id/commentPost', commentPost);
-
 export default router;
